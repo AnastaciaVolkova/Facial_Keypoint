4a69e2547a0605d40d3d9f99c634e799de86c66c
AnastaciaVolkova <polarbolide@gmail.com>
2020-07-25 10:50:29 +0200

    feat: Add convolutional layer

--- models.py
+++ models.py
@@ -22,16 +22,18 @@ class Net(nn.Module):
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        w2 = 13 
+        w2 = 5 
        
         linear_size_in = w2*w2*256
         linear_size_out = w2*256 
         linear_size_out_2 = 136 
  
-        self.conv1 = nn.Conv2d(1, 32, 4)
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        self.conv3 = nn.Conv2d(64, 128, 2)
-        self.conv4 = nn.Conv2d(128, 256, 1)
+        self.conv1 = nn.Conv2d(1, 32, 7)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.conv5 = nn.Conv2d(256, 256, 1)
+
         nn.init.xavier_uniform_(self.conv1.weight)
         nn.init.xavier_uniform_(self.conv2.weight)
         nn.init.xavier_uniform_(self.conv3.weight)
@@ -54,14 +56,15 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         flat = nn.Flatten()
-        x = F.dropout(F.max_pool2d(F.elu(self.conv1(x)), kernel_size=2, stride=2), p=0.1)
-        x = F.dropout(F.max_pool2d(F.elu(self.conv2(x)), kernel_size=2, stride=2), p=0.2)
-        x = F.dropout(F.max_pool2d(F.elu(self.conv3(x)), kernel_size=2, stride=2), p=0.3)
-        x = F.dropout(F.max_pool2d(F.elu(self.conv4(x)), kernel_size=2, stride=2), p=0.4)
-       
-        x = flat(x) 
+        x = F.dropout(F.max_pool2d(F.relu(self.conv1(x)), kernel_size=2, stride=2), p=0.1)
+        x = F.dropout(F.max_pool2d(F.relu(self.conv2(x)), kernel_size=2, stride=2), p=0.1)
+        x = F.dropout(F.max_pool2d(F.relu(self.conv3(x)), kernel_size=2, stride=2), p=0.2)
+        x = F.dropout(F.max_pool2d(F.relu(self.conv4(x)), kernel_size=2, stride=2), p=0.3)
+        x = F.dropout(F.max_pool2d(F.relu(self.conv5(x)), kernel_size=2, stride=2), p=0.4)
 
-        x = F.dropout(F.elu(self.dense1(x)), p=0.5)
+        x = flat(x) 
+        
+        x = F.dropout(F.relu(self.dense1(x)), p=0.5)
         x = F.dropout(self.act(self.dense2(x)), p=0.6)
 
         x = self.dense3(x)
